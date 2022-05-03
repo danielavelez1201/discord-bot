2240ce2d64a0203cff9cbf2f776e9bc13f304c5b
Ivan <ivanzhangofficial@gmail.com>
2022-05-03 14:50:25 -0400

    update parse script

--- db/functions/db_fetch.py
+++ db/functions/db_fetch.py
@@ -12,6 +12,7 @@ def get_user_id_from_answer_id(answer_id):
     result = cursor.fetchall()
     return result[0]
 
+
 # From question corresponding answer
 def get_user_id_from_question_id_for_answer(answer_id):
     result = get_question_id_from_answer_id(answer_id)
@@ -23,6 +24,7 @@ def get_user_id_from_question_id_for_answer(answer_id):
         return result[0]
     return None
 
+
 # From message
 def get_user_id_from_message_id(message_id):
     request_sql = "SELECT author_id FROM messages WHERE id = " + str(message_id)
@@ -30,11 +32,13 @@ def get_user_id_from_message_id(message_id):
     result = cursor.fetchall()
     return result[0]
 
+
 # Name, nickname from id
 def get_user_with_id(id):
     user_sql = f"SELECT name, nick FROM users WHERE id = {id}"
     cursor.execute(user_sql)
-    return cursor.fetchall()[0]    
+    return cursor.fetchall()[0]
+
 
 """
 Retrieving question, message, and answer id and info.
@@ -47,6 +51,7 @@ def get_question_id_from_answer_id(answer_id):
     result = cursor.fetchall()
     return result[0]
 
+
 # Question tuple from id
 def get_question_with_id(id):
     question_sql = f"SELECT id, author_id, title, body, upvotes, answered FROM questions WHERE id = {id}"
@@ -55,6 +60,7 @@ def get_question_with_id(id):
     if len(result) == 0:
         return None
     return result[0]
+
 
 # Message tuple from id, in question tuple format
 def get_message_with_id_with_question_format(id):
@@ -66,11 +72,13 @@ def get_message_with_id_with_question_format(id):
     id, author_id, text, upvotes = result[0]
     return (id, author_id, "", text, upvotes, False)
 
+
 # Question/message tuple from id
 def get_question_or_message_with_id(id):
     question_query = get_question_with_id(id)
     message_query = get_message_with_id_with_question_format(id)
     return question_query if question_query else message_query
+
 
 # Answer corresponding to question id
 def get_answer_with_question_id(id):
@@ -85,29 +93,35 @@ Retrieving relevant questions, messages, and answers.
 
 # Questions
 def get_question_ids_with_keyword(keyword):
-    question_sql = f"SELECT question_id FROM keywords_questions WHERE word = \"{keyword}\""
+    question_sql = (
+        f'SELECT question_id FROM keywords_questions WHERE word = "{keyword}"'
+    )
     cursor.execute(question_sql)
     question_ids = cursor.fetchall()
     return question_ids
 
+
 # Messages
 def get_message_ids_with_keyword(keyword):
-    message_sql = f"SELECT message_id FROM keywords_messages WHERE word = \"{keyword}\""
+    message_sql = f'SELECT message_id FROM keywords_messages WHERE word = "{keyword}"'
     cursor.execute(message_sql)
     message_ids = cursor.fetchall()
     return message_ids
 
+
 # Answers
 def get_answer_ids_with_keyword(keyword):
-    answer_sql = f"SELECT answer_id FROM keywords_answers WHERE word = \"{keyword}\""
+    answer_sql = f'SELECT answer_id FROM keywords_answers WHERE word = "{keyword}"'
     cursor.execute(answer_sql)
     answer_ids = cursor.fetchall()
     return answer_ids
+
 
 # Questions based on answers
 def get_question_ids_with_relevant_answers(keyword):
     answer_ids = get_answer_ids_with_keyword(keyword)
     return [get_question_id_from_answer_id(id[0]) for id in answer_ids]
+
 
 """
 Retrieving contributor info.
@@ -120,6 +134,7 @@ def get_score_from_author(user_id):
     result = cursor.fetchall()
     return result
 
+
 # Top contributors
 def get_top_contributors(count):
     request_sql = "SELECT * FROM users ORDER BY contribution_score ASC LIMIT " + str(
@@ -129,15 +144,18 @@ def get_top_contributors(count):
     result = cursor.fetchall()
     return result
 
+
 """
 Retrieving all messages.
 """
+
 
 def all_messages():
     request_sql = "SELECT * FROM messages"
     cursor.execute(request_sql)
     result = cursor.fetchall()
     return result
+
 
 def messagesFormatted():
     request_sql = """
@@ -158,3 +176,12 @@ def messagesFormatted():
             result += author_name + ": "
             result += text + "\n"
     return result
+
+
+def getMessages():
+    request_sql = """
+        SELECT * FROM messages ORDER BY created_at DESC;
+    """
+    cursor.execute(request_sql)
+    messages = cursor.fetchall()
+    return messages
--- parse-message-history.py
+++ parse-message-history.py
@@ -1,5 +1,13 @@
 from utils.gpt3 import extract_keywords
-from db.functions.db_update import addKeywordsToDB, addMessage, addMessageIDToKeywords, addUser, addServer
+from db.functions.db_update import (
+    addKeywordsToDB,
+    addMessage,
+    addMessageIDToKeywords,
+    addUser,
+    addServer,
+)
+
+from db.functions.db_fetch import getMessages
 
 f = open("near-messages.txt", "r")
 
@@ -10,23 +18,42 @@ text = text.split(";;;;")
 text = text[1:]
 text = text[:-1]
 
-#message_id, message_content, user_id, timestamp
+# message_id, message_content, user_id, timestamp
+
 
 def parse_and_get_keywords():
     addServer(490367152054992913, "", -1)
     counter = 0
-    for line in text: 
+    for line in text:
         lineArr = line.split(",")
-        messageId = lineArr[0]
+        messageId = lineArr[0].strip()
         timestamp = lineArr[-1]
         userId = lineArr[-2]
         content = lineArr[1:-2]
         content = "".join(content)
-        keywords = extract_keywords(content)
         addUser(int(userId), "", "", 490367152054992913)
-        addKeywordsToDB(keywords)
         addMessage(int(messageId), int(userId), 490367152054992913, content, 0)
-        addMessageIDToKeywords(int(messageId), keywords)
         counter += 1
+
+
+def get_keywords():
+    count = 10
+    messages = getMessages()[0:5]
+    for (
+        message_id,
+        author_id,
+        server_id,
+        text,
+        upvotes,
+        created_at,
+        updated_at,
+    ) in messages:
+        keywords = extract_keywords(text)
+        addKeywordsToDB(keywords)
+        addMessageIDToKeywords(int(message_id), keywords)
+        count += 1
+        print(count)
+    return
+
 
 parse_and_get_keywords()
