e07e2d03e9ceafd42838711adf4f420434546ff6
Daniela Velez <danielavelez1201@gmail.com>
2022-05-03 15:46:52 -0400

    Move around data folders and finish up discord scraping code:

--- db/scripts/generate_from_csv.py
+++ db/scripts/generate_from_csv.py
@@ -18,22 +18,25 @@ def generate_from_csv():
         "keywords_answers",
     ]
     for table in tables:
-        with open("data/" + table + ".csv", "r") as table_data:
-            header, *rows = table_data.readlines()
-        col_names = [name.strip() for name in header.split(delimiter)]
-        col_sql = "(" + ",".join(col_names) + ")"
-        value_slots = ", ".join(["%s" for i in range(len(col_names))])
-        for row in rows:
-            row_values = [value.strip() for value in row.split(delimiter)]
-            insert_sql = "INSERT IGNORE INTO {} {} VALUES ({})".format(
-                table, col_sql, value_slots
-            )
-            try:
-                cursor.execute(insert_sql, row_values)
-                cnx.commit()
-                print(table, "DONE")
-            except Error as err:
-                print("Tried adding answer: {}".format(err))
+        try:
+            with open("../../data/" + table + ".csv", "r") as table_data:
+                header, *rows = table_data.readlines()
+            col_names = [name.strip() for name in header.split(delimiter)]
+            col_sql = "(" + ",".join(col_names) + ")"
+            value_slots = ", ".join(["%s" for i in range(len(col_names))])
+            for row in rows:
+                row_values = [value.strip() for value in row.split(delimiter)]
+                insert_sql = "INSERT IGNORE INTO {} {} VALUES ({})".format(
+                    table, col_sql, value_slots
+                )
+                try:
+                    cursor.execute(insert_sql, row_values)
+                    cnx.commit()
+                    print(table, "DONE")
+                except Error as err:
+                    print("Tried adding answer: {}".format(err))
+        except Error as err:
+            print('skipped because of: ', err)
     return
 
 
---
+++ db/scripts/scrape_discords.py
@@ -0,0 +1,62 @@
+import csv 
+import requests
+import json
+
+def get_server_ids():
+    result = []
+    with open('../../general_data/server_ids.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ',')
+        for row in reader:
+            if row[0] == '':
+                return result
+            if row[6] != '' and row[6] != "#N/A":
+                result.append({
+                    'name': row[0],
+                    'id': row[6]
+                })
+    return result
+     
+def retrieve_messages(server_info):
+    channel_id = server_info['id']
+    channel_name = server_info['name']
+    print(channel_name, channel_id)
+
+    num = 0
+    limit = 100
+
+    headers = {
+        'authorization': 'Bearer TOKEN'
+    }
+
+    last_message_id = None
+    retString = "message_id, message_content, user_id, timestamp\n"
+
+    query_parameters = f'limit={limit}'
+    if last_message_id is not None:
+        query_parameters += f'&before={last_message_id}'
+
+    r = requests.get(
+        f'https://discord.com/api/v9/channels/{channel_id}/messages?{query_parameters}',headers=headers
+        )
+    jsonn = json.loads(r.text)
+    print(jsonn)
+    for value in jsonn:
+        retString += value['id'] + "," + value['content'] + "," + value['author']['id'] + "," + value['timestamp'] + ";;;; \n"
+        #print(value['content'], '\n')
+        last_message_id = value['id']
+        num=num+1
+    print('number of messages we collected is',num)
+    print(retString)
+    with open(f'{channel_name}-messages.txt', 'w') as f:
+        f.write(retString)
+
+
+def scrape_discord_servers():
+    servers = get_server_ids()
+    for server in servers:
+        try:
+            retrieve_messages(server)
+        except Exception as err:
+            print("skipped this server: ", err)
+        
+scrape_discord_servers()
